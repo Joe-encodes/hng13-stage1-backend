54e19bcfc13db9a01d6a9a4265caf4e6f090064f
techyJcrafts <joseph.adamu.it@gmail.com>
2025-10-23 01:12:55 +0100

    feat: enhance routing in Slim application with additional endpoints
    - Added new GET endpoints for filtering strings by natural language and retrieving strings by value.
    - Implemented a DELETE endpoint for string removal.
    - Introduced a default route to check application status.
    - Updated the StringController and FilterController usage in the routing setup.

--- public/index.php
+++ public/index.php
@@ -1,17 +1,27 @@
 <?php
 use Slim\Factory\AppFactory;
-use Illuminate\Database\Capsule\Manager as DB;
+use App\Controllers\StringController;
+use App\Controllers\FilterController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/StringService.php';
-require __DIR__ . '/../src/Controllers/StringController.php';
 
 $capsule = require __DIR__ . '/../src/Database.php';
 
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 
-// POST /strings
-$app->post('/strings', [StringController::class, 'create']);
+$app->get('/strings/filter-by-natural-language', [FilterController::class, 'filterByNatural']); 
+$app->get('/strings', [FilterController::class, 'filter']);
+$app->post('/strings', [StringController::class, 'create']); 
+$app->get('/strings/{string_value}', [StringController::class, 'getByValue']);
+$app->delete('/strings/{string_value}', [StringController::class, 'delete']);
+
+// Default
+$app->get('/', function (Request $request, Response $res) {
+    $res->getBody()->write(json_encode(["status" => "running"], JSON_PRETTY_PRINT));
+    return $res->withHeader('Content-Type', 'application/json');
+});
 
 $app->run();
